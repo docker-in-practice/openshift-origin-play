d2504526771ec0b20f8f4cbbe12c4952cd89dc4f
Ian Miell <ian.miell@gmail.com>
2015-03-26 08:33:33 +0000

    merged

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 NAME=openshift-origin
-docker pull imiell/openshift-origin
+docker pull dockerinpractice/openshift-origin-play
 mkdir -p /tmp/openshift
 echo "================================================"
 echo "Trying to remove any existing container running"
 echo "================================================"
 docker rm -f $NAME || /bin/true
-docker run -d -v /tmp/openshift:/tmp/openshift --name $NAME --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock imiell/openshift-origin start
+docker run -d --name $NAME --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock dockerinpractice/openshift-origin-play start
 echo "================================================"
 echo "You are now in the origin server container"
 echo "================================================"
