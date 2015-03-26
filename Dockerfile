6ba65d2c55ce6325e72eef5b889304e330133eb6
Ian Miell <ian.miell@gmail.com>
2015-03-26 08:14:22 +0000

    util

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM openshift/origin
 
-RUN yum install -y docker-io unzip telnet nmap git
+RUN yum install -y docker-io unzip telnet nmap git mysql-client
 RUN mkdir /git
 RUN git clone https://github.com/openshift/training /git/training
 RUN git clone https://github.com/GoogleCloudPlatform/kubernetes /git/kubernetes
+RUN git clone https://github.com/aidanhs/dockerutil
 
--- run.sh
+++ run.sh
@@ -8,5 +8,8 @@ echo "Trying to remove any existing container running"
 echo "================================================"
 docker rm -f $NAME || /bin/true
 docker run -d -v /tmp/openshift:/tmp/openshift --name $NAME --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock imiell/openshift-origin start
+echo "================================================"
+echo "You are now in the origin server container"
+echo "================================================"
 docker exec -ti $NAME bash
 
